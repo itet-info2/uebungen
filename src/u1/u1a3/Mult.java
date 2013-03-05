5011066a3a3ab0e1af9f366cd5b91c05ea1b2fa6
Linus Basig <linus@basig.me>
2013-03-05 22:27:00 +0100

    u1a3: Add documentation

--- src/u1/u1a3/Mult.java
+++ src/u1/u1a3/Mult.java
@@ -10,9 +10,19 @@ public class Mult {
 			return a + f(2 * a, (b - 1) / 2);
 	}
 
+	/**
+	 * Multipliziert zwei Zahlen
+	 * 
+	 * @author Linus Basig
+	 * @param a Zahl > 0
+	 * @param b Zahl > 0
+	 * @exception IllegalArgumentException Löst bei a oder b > 0 aus
+	 * @return a*b
+	 */
 	public static int mult(int a, int b) {
 		if (a < 1 || b < 1) {
-			throw new IllegalArgumentException("a(" + a + ") or b(" + b + ") < 1");
+			throw new IllegalArgumentException("a(" + a + ") or b(" + b
+					+ ") < 1");
 		}
 		return f(a, b);
 	}
