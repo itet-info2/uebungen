b1431241be1b441118de38a5145407f48841b7b6
Linus Basig <linus@basig.me>
2013-03-05 21:48:54 +0100

    u1a3 complete

--- src/u1/u1a3/Mult.java
+++ src/u1/u1a3/Mult.java
@@ -1,14 +1,19 @@
 package u1.u1a3;
 
 public class Mult {
-    private static int f(int a, int b) {
-        if (b==0) return a;
-        if (b%2 == 0) return f(2*a, b/2);
-        else return a + f(2*a, b/2);
-    }
-    
-    public static int mult(int a, int b) {
+	private static int f(int a, int b) {
+		if (b == 1)
+			return a;
+		if (b % 2 == 0)
+			return f(2 * a, b / 2);
+		else
+			return a + f(2 * a, (b - 1) / 2);
+	}
 
-        return f(a, b);
-    }
+	public static int mult(int a, int b) {
+		if (a < 1 || b < 1) {
+			throw new IllegalArgumentException("a(" + a + ") or b(" + b + ") < 1");
+		}
+		return f(a, b);
+	}
 }
